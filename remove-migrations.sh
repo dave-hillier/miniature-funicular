6bb8dec16986ddb4fee2dd254bf781aba3843ccf
Dave Hillier <dave@hillier.co>
2019-05-30 21:56:32 +0100

    Added remove migrations script

---
+++ remove-migrations.sh
@@ -0,0 +1,12 @@
+cd Issues
+dotnet ef migrations remove
+cd -
+cd Properties
+dotnet ef migrations remove
+cd -
+cd Tasks
+dotnet ef migrations remove
+cd -
+cd Users
+dotnet ef migrations remove
+cd -
